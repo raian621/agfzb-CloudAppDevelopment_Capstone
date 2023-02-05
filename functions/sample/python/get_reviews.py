be858f6354be0e3600205591b3d7c6c6787477e2
Bell, Ryan <ryan.bell62101@gmail.com>
2023-02-04 23:41:04 -0600

    finished django models

--- functions/sample/python/get_reviews.py
+++ functions/sample/python/get_reviews.py
@@ -4,35 +4,44 @@ from ibmcloudant.cloudant_v1 import CloudantV1
 import requests
 
 def main(param_dict):
-  auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
-  client = CloudantV1(authenticator=auth)
-  client.authenticator = auth
-  client.set_service_url(param_dict["COUCH_URL"])
-  
-  result = None
-  selector = {}
+	"""
+	returns the reviews for a dealership
+	
+	filters reviews by dealerId:
+	- GET <domain>/api/review?dealerId=<int dealerId>
 
-  if "dealerId" in param_dict:
-    selector["dealership"] = {
-      "$eq": param_dict["dealerId"]
-    }
+	post review
+	- GET <domain>/api/review
+	"""
+	auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
+	client = CloudantV1(authenticator=auth)
+	client.authenticator = auth
+	client.set_service_url(param_dict["COUCH_URL"])
+	
+	result = None
+	selector = {}
 
-  try:
-    result = client.post_find(db=param_dict["COUCH_DATABASE"], limit=10, selector=selector).get_result()["docs"]
-  except ApiException as ae:
-    print("unable to connect", ae)
-    return {
-      'statusCode': 404,
-      'message': 'Something went wrong'
-    }
-  except (requests.exceptions.RequestException, ConnectionResetError) as err:
-    print("connection error", err)
-    return {
-      'statusCode': 404,
-      'message': 'Something went wrong'
-    }
-  
-  return {
-    "headers": { "Content-Type":"application/json" },
-    "body": { "data": result }
-  }
+	if "dealerId" in param_dict:
+		selector["dealership"] = {
+			"$eq": param_dict["dealerId"]
+		}
+
+	try:
+		result = client.post_find(db=param_dict["COUCH_DATABASE"], limit=10, selector=selector).get_result()["docs"]
+	except ApiException as api_exception:
+		print("unable to connect", api_exception)
+		return {
+			'statusCode': 404,
+			'message': 'Something went wrong'
+		}
+	except (requests.exceptions.RequestException, ConnectionResetError) as err:
+		print("connection error", err)
+		return {
+			'statusCode': 404,
+			'message': 'Something went wrong'
+		}
+	
+	return {
+		"headers": { "Content-Type":"application/json" },
+		"body": { "data": result }
+	}
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+  name = models.CharField(max_length=50)
+  description = models.CharField(max_length=100)
+
+  def __str__(self):
+    return f"{self.name} - {self.description[0:20]}"
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +25,20 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+  SEDAN = "SE"
+  SUV = "SU"
+  WAGON = "WA"
+
+  car_make = models.ForeignKey(to=CarMake, on_delete=models.CASCADE)
+  dealer_id = models.IntegerField()
+  name = models.CharField(max_length=50)
+  type = models.CharField(max_length=20, choices=[
+    (SEDAN, "Sedan"), 
+    (SUV, "SUV"), 
+    (WAGON, "WAGON")
+  ])
+  year = models.DateField()
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
