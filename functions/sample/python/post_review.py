9d76bd746543f99cda8dda38f0336aa3f411f41a
Bell, Ryan <ryan.bell62101@gmail.com>
2023-02-04 18:32:16 -0600

    fixed indentation for pylint

--- functions/sample/python/post_review.py
+++ functions/sample/python/post_review.py
@@ -5,29 +5,29 @@ import requests
 
 
 def main(param_dict):
-  auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
-  client = CloudantV1(authenticator=auth)
-  client.authenticator = auth
-  client.set_service_url(param_dict["COUCH_URL"])
-  
-  result = None
-  
-  try:
-    result = client.post_document(db=param_dict["COUCH_DATABASE"], document=param_dict["review"]).get_result()
-  except ApiException as ae:
-    print("unable to connect", ae)
-    return {
-      'statusCode': 404,
-      'message': 'Something went wrong'
-    }
-  except (requests.exceptions.RequestException, ConnectionResetError) as err:
-    print("connection error", err)
-    return {
-      'statusCode': 404,
-      'message': 'Something went wrong'
-    }
+	auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
+	client = CloudantV1(authenticator=auth)
+	client.authenticator = auth
+	client.set_service_url(param_dict["COUCH_URL"])
+	
+	result = None
+	
+	try:
+		result = client.post_document(db=param_dict["COUCH_DATABASE"], document=param_dict["review"]).get_result()
+	except ApiException as ae:
+		print("unable to connect", ae)
+		return {
+		'statusCode': 404,
+		'message': 'Something went wrong'
+		}
+	except (requests.exceptions.RequestException, ConnectionResetError) as err:
+		print("connection error", err)
+		return {
+		'statusCode': 404,
+		'message': 'Something went wrong'
+		}
 
-  return {
-    "headers": { "Content-Type":"application/json" },
-    "body": { "data": result }
-  }
+	return {
+		"headers": { "Content-Type":"application/json" },
+		"body": { "data": result }
+	}
