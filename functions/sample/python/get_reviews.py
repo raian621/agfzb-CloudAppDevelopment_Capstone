e187e972ce163cba4fd5fb6a153496a5ad13e9b3
Bell, Ryan <ryan.bell62101@gmail.com>
2024-03-13 18:32:50 -0500

    fixed pylint issues in for cloud functions

--- functions/sample/python/get_reviews.py
+++ functions/sample/python/get_reviews.py
@@ -1,47 +1,55 @@
+"""
+Cloud function used to read reviews from the Cloudant database
+"""
+
 from ibm_cloud_sdk_core import ApiException
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibmcloudant.cloudant_v1 import CloudantV1
 import requests
 
 def main(param_dict):
-	"""
-	returns the reviews for a dealership
-	
-	filters reviews by dealerId:
-	- GET <domain>/api/review?dealerId=<int dealerId>
+    """
+    returns the reviews for a dealership
 
-	post review
-	- GET <domain>/api/review
-	"""
-	auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
-	client = CloudantV1(authenticator=auth)
-	client.authenticator = auth
-	client.set_service_url(param_dict["COUCH_URL"])
-	
-	result = None
-	selector = {}
+    filters reviews by dealerId:
+    - GET <domain>/api/review?dealerId=<int dealerId>
 
-	if "dealerId" in param_dict:
-		selector["dealership"] = {
-			"$eq": param_dict["dealerId"]
-		}
+    post review
+    - GET <domain>/api/review
+    """
+    auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
+    client = CloudantV1(authenticator=auth)
+    client.authenticator = auth
+    client.set_service_url(param_dict["COUCH_URL"])
 
-	try:
-		result = client.post_find(db=param_dict["COUCH_DATABASE"], limit=10, selector=selector).get_result()["docs"]
-	except ApiException as api_exception:
-		print("unable to connect", api_exception)
-		return {
-			'statusCode': 404,
-			'message': 'Something went wrong'
-		}
-	except (requests.exceptions.RequestException, ConnectionResetError) as err:
-		print("connection error", err)
-		return {
-			'statusCode': 404,
-			'message': 'Something went wrong'
-		}
-	
-	return {
-		"headers": { "Content-Type":"application/json" },
-		"body": { "data": result }
-	}
+    result = None
+    selector = {}
+
+    if "dealerId" in param_dict:
+        selector["dealership"] = {
+            "$eq": param_dict["dealerId"]
+        }
+
+    try:
+        result = client.post_find(
+            db=param_dict["COUCH_DATABASE"],
+            limit=10,
+            selector=selector
+        ).get_result()["docs"]
+    except ApiException as api_exception:
+        print("unable to connect", api_exception)
+        return {
+            'statusCode': 404,
+            'message': 'Something went wrong'
+        }
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("connection error", err)
+        return {
+            'statusCode': 404,
+            'message': 'Something went wrong'
+        }
+
+    return {
+        "headers": { "Content-Type":"application/json" },
+        "body": { "data": result }
+    }
--- functions/sample/python/post_review.py
+++ functions/sample/python/post_review.py
@@ -1,3 +1,7 @@
+"""
+Cloud function used to add reviews to the Cloudant database
+"""
+
 from ibm_cloud_sdk_core import ApiException
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibmcloudant.cloudant_v1 import CloudantV1
@@ -5,29 +9,40 @@ import requests
 
 
 def main(param_dict):
-	auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
-	client = CloudantV1(authenticator=auth)
-	client.authenticator = auth
-	client.set_service_url(param_dict["COUCH_URL"])
-	
-	result = None
-	
-	try:
-		result = client.post_document(db=param_dict["COUCH_DATABASE"], document=param_dict["review"]).get_result()
-	except ApiException as ae:
-		print("unable to connect", ae)
-		return {
-		'statusCode': 404,
-		'message': 'Something went wrong'
-		}
-	except (requests.exceptions.RequestException, ConnectionResetError) as err:
-		print("connection error", err)
-		return {
-		'statusCode': 404,
-		'message': 'Something went wrong'
-		}
+    """
+    adds a review to the Cloudant database
+    
+    - POST <domain>/api/review
+    {
+        "review": { data... }
+    }
+    """
+    auth = IAMAuthenticator(apikey=param_dict["IAM_API_KEY"])
+    client = CloudantV1(authenticator=auth)
+    client.authenticator = auth
+    client.set_service_url(param_dict["COUCH_URL"])
 
-	return {
-		"headers": { "Content-Type":"application/json" },
-		"body": { "data": result }
-	}
+    result = None
+
+    try:
+        result = client.post_document(
+            db=param_dict["COUCH_DATABASE"],
+            document=param_dict["review"]
+        ).get_result()
+    except ApiException as ae:
+        print("unable to connect", ae)
+        return {
+            'statusCode': 404,
+            'message': 'Something went wrong'
+        }
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("connection error", err)
+        return {
+            'statusCode': 404,
+            'message': 'Something went wrong'
+        }
+
+    return {
+        "headers": { "Content-Type":"application/json" },
+        "body": { "data": result }
+    }
